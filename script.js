98d7928caca4110458f3ec4078ac89a411b0109d
amacwan2428 <110256216+amacwan2428@users.noreply.github.com>
2022-10-21 18:32:56 -0400

    Added

---
+++ script.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("myModal");
+function modalFunc(){
+    var modal = document.getElementById("myModal");
+      modal.style.display = "block";
+
+}
+
+
+
+// When the user clicks on <span> (x), close the modal
+
+
+// When the user clicks anywhere outside of the modal, close it
+
+
+function closeModal(){
+    var modal = document.getElementById("myModal");
+ modal.style.display="none";   
+}
